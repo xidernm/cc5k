bd5a2dff4fd668313601c0619e3ab4a24ff85275
Justin Edwards <xidernm@unm.edu>
2014-03-15 11:46:43 -0600

    Added first and last name and zipcode to user signups

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   if current_user!=nil
         @areaGraph =LazyHighCharts::HighChart.new('area') do |f|
         f.series(:name=>'Average',:data=>[2, 19, 4, 30] ) 
-        f.series(:name=>current_user.email,:data=> [10, 20, 12,10])
+        f.series(:name=>current_user.firstName,:data=> [10, 20, 12,10])
         f.title({ :text=>"Carbon Usage Compared to Regional Average"})
         f.options[:chart][:defaultSeriesType] = "area"
         f.options[:xAxis][:categories] = ['Vehicle', 'Natural Gas', 'Electric','Food']
@@ -19,7 +19,7 @@ class HomeController < ApplicationController
         f.series(:name=>'Electrical',:data=>[25,20,30]) 
         f.title({ :text=>"Carbon Usage Compared to Regional Average"})
         f.options[:chart][:defaultSeriesType] = "column"
-        f.options[:xAxis][:categories] = ['National Average', current_user.email, 'Regional Average']
+        f.options[:xAxis][:categories] = ['National Average', current_user.firstName, 'Regional Average']
         f.colors
         f.plot_options({:column=>{:stacking=>"percent"}})
         end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140305044829) do
+ActiveRecord::Schema.define(version: 20140315174105) do
 
   create_table "answered_factors", force: true do |t|
     t.integer  "factor_id"
@@ -100,6 +100,9 @@ ActiveRecord::Schema.define(version: 20140305044829) do
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
     t.integer  "region_id"
+    t.string   "firstName"
+    t.string   "lastName"
+    t.integer  "zip"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
