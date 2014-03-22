df3e867bd73a70f6f355dbdb9856d93156fb674c
Justin Edwards <xidernm@unm.edu>
2014-03-22 15:08:55 -0600

    more oops

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,40 +2,40 @@ class HomeController < ApplicationController
 
   def index
     
-  if current_user!=nil
-        fieldMap = Hash.new
-       
-        ans = Answer.where(user_id: current_user.id)
-        ans.each do |a|
-        fieldMap[Statistic.find(a.statistic_id).description] = a.amount
-        end
-
-        if fieldMap.first = nil
-          @areaGraph = "NO GRAPH"
-          @distributionGraph = "NO GRAPH"
-        else
-          @areaGraph =LazyHighCharts::HighChart.new('area') do |f|
-          f.series(:name=>current_user.firstName,:data=> fieldMap.values) # << Now we can query the Answer table for the user
-          f.title({ :text=>"Carbon Usage By Category"})
-          f.options[:chart][:defaultSeriesType] = "area"
-          f.options[:xAxis][:categories] = fieldMap.keys
-          f.colors
-          f.plot_options({:column=>{:stacking=>"percent"}})
-          end
-        end
+#  if current_user!=nil
+#        fieldMap = Hash.new
+#       
+#        ans = Answer.where(user_id: current_user.id)
+#        ans.each do |a|
+#        fieldMap[Statistic.find(a.statistic_id).description] = a.amount
+#        end
+#
+#        if fieldMap.first = nil
+#          @areaGraph = "NO GRAPH"
+#          @distributionGraph = "NO GRAPH"
+#        else
+#          @areaGraph =LazyHighCharts::HighChart.new('area') do |f|
+#          f.series(:name=>current_user.firstName,:data=> fieldMap.values) # << Now we can query the Answer table for the user
+#          f.title({ :text=>"Carbon Usage By Category"})
+#          f.options[:chart][:defaultSeriesType] = "area"
+#          f.options[:xAxis][:categories] = fieldMap.keys
+#          f.colors
+#          f.plot_options({:column=>{:stacking=>"percent"}})
+#          end
+#        end
     
-        @distributionGraph =LazyHighCharts::HighChart.new('column') do |f|
-        fieldMap.each do |m|
-        list = [10,m[1],20]
-        f.series(:name=>m[0],:data=>list )  
-        end
-        f.title({ :text=>"Carbon Usage Compared to Regional Average"})
-        f.options[:chart][:defaultSeriesType] = "column"
-        f.options[:xAxis][:categories] = ['National Average', current_user.firstName, 'Regional Average']
-        f.colors
-        f.plot_options({:column=>{:stacking=>"percent"}})
-        end
-    end
+ #       @distributionGraph =LazyHighCharts::HighChart.new('column') do |f|
+#        fieldMap.each do |m|
+#        list = [10,m[1],20]
+#        f.series(:name=>m[0],:data=>list )  
+#        end
+#        f.title({ :text=>"Carbon Usage Compared to Regional Average"})
+#        f.options[:chart][:defaultSeriesType] = "column"
+#        f.options[:xAxis][:categories] = ['National Average', current_user.firstName, 'Regional Average']
+#        f.colors
+#        f.plot_options({:column=>{:stacking=>"percent"}})
+#        end
+#    end
   end
   private
   def getAnsweredFactors
