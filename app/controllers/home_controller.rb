222d7db13ae36e8260e8819e489c6552668ad732
beenjaminmb <beenjaminmb@gmail.com>
2014-03-22 15:08:50 -0600

    Home page issues

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,21 +2,21 @@ class HomeController < ApplicationController
 
   def index
     
-  if current_user!=nil
-
-        @ans = Answer.where(user_id: current_user.id)
-        @ans.each do |a|
+    if current_user!=nil
+      
+      @ans = Answer.where(user_id: current_user.id)
+      @ans.each do |a|
         a.name = Statistic.find(a.statistic_id).description
-        end
-        @areaGraph =LazyHighCharts::HighChart.new('area') do |f|
+      end
+      @areaGraph =LazyHighCharts::HighChart.new('area') do |f|
         f.series(:name=>current_user.firstName,:data=> [10, 20, 12,10]) # << Now we can query the Answer table for the user
         f.title({ :text=>"Carbon Usage Compared to Regional Average"})
         f.options[:chart][:defaultSeriesType] = "area"
         f.options[:xAxis][:categories] = ['Vehicle', 'Natural Gas', 'Electric','Food']
         f.colors
         f.plot_options({:column=>{:stacking=>"percent"}})
-        end
-        @distributionGraph =LazyHighCharts::HighChart.new('column') do |f|
+      end
+      @distributionGraph =LazyHighCharts::HighChart.new('column') do |f|
         f.series(:name=>'Vehicle',:data=>[10,12,9] )  
         f.series(:name=>'Food',:data=>[10,10,10] ) 
         f.series(:name=>'Heating',:data=>[25,20,30] ) 
@@ -26,7 +26,7 @@ class HomeController < ApplicationController
         f.options[:xAxis][:categories] = ['National Average', current_user.firstName, 'Regional Average']
         f.colors
         f.plot_options({:column=>{:stacking=>"percent"}})
-        end
+      end
     end
   end
   private
