2e5966d0b7a3be5d042ac882c2616814c1d6f9c2
beenjaminmb <beenjaminmb@gmail.com>
2014-04-09 09:35:37 -0600

    Added a line of code in the application_controller that I removed earlier.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
