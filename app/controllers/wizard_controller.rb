b760ba73e1883a8e6a3db8944bfde175591de647
beenjaminmb <beenjaminmb@gmail.com>
2014-04-18 18:44:35 -0600

    Bug fixing in missions index

--- app/controllers/wizard_controller.rb
+++ app/controllers/wizard_controller.rb
@@ -2,7 +2,7 @@ class WizardController < ApplicationController
 
   def index
     mid = params[:format]
-    @mission = Mission.find_by(mid)
+    @mission = Mission.find_by(id: mid)
     @factors = Factor.all
     @categories = Category.all
     @date = Time.new
