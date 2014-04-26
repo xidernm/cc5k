063925abc2bf9b8526a832d2c59f4c0c7a11cc12
Sixstring982 <sixstring982@gmail.com>
2014-04-26 15:27:47 -0600

    Equations can now handle parentheses.

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -1,30 +1,54 @@
 class Statistic < ActiveRecord::Base
   has_many :factors, dependent: :destroy
   has_many :answers, dependent: :destroy
-  
+
   def self.IsValidEquation(eqstr)
-    /[A-Z]([+\-*\/][A-Z])*/ =~ eqstr.gsub(/\s+/,"")
+    /[A-Z]([\(\)+\-*\/][A-Z])*/ =~ eqstr.gsub(/\s+/,"")
   end
 
-  def equationToRpn
+  # def equationToRpn
+  #   stack = []
+  #   queue = []
+  #   precedence = {'+'.to_s => 2, '-'.to_s => 2, '/'.to_s => 3, '*'.to_s => 3}
+
+  #   equation.each_char do |c|
+  #     if /[A-Z]/ =~ c
+  #       queue << c
+  #     else
+  #       while !stack.empty? and precedence[stack.last] == precedence[c]
+  #         queue << stack.pop
+  #       end
+  #       stack.push(c)
+  #     end
+  #   end
+  #   (queue + stack.reverse).join
+  # end
+
+  def self.equationToRpn(equation)
     stack = []
     queue = []
     precedence = {'+'.to_s => 2, '-'.to_s => 2, '/'.to_s => 3, '*'.to_s => 3}
-    
+
     equation.each_char do |c|
       if /[A-Z]/ =~ c
         queue << c
-      else
-        while !stack.empty? and precedence[stack.last] == precedence[c]
+      elsif /[+\-*\/]/ =~ c
+        while !stack.empty? && /[+\-*\/]/ =~ stack.last && precedence[stack.last] >= precedence[c]
           queue << stack.pop
         end
         stack.push(c)
+      elsif c == '('
+        stack.push(c)
+      elsif c == ')'
+        while stack.last != '('
+          queue << stack.pop
+        end
+        stack.pop
       end
     end
     (queue + stack.reverse).join
   end
 
-  
   def replaceName(factors, ch)
     factor = nil
     factors.each do |af|
@@ -37,9 +61,9 @@ class Statistic < ActiveRecord::Base
     end
     return nil
   end
-  
+
   def EvalEquation(current_user, pairs)
-    eqn = equationToRpn
+    eqn = equationToRpn(self.equation)
     stack = []
     ops = {"+" => lambda {|x,y| x + y},
            "-" => lambda {|x,y| x - y},
@@ -51,7 +75,7 @@ class Statistic < ActiveRecord::Base
         num = replaceName(pairs, c)
         # The logic here needs to be modified in order to replace variable names with values.
         if num != nil
-          stack.push(num) 
+          stack.push(num)
         else
           num = Factor.find_by(statistic_id: id, name: c).amount
           stack.push(num)
